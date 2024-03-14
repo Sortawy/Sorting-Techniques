63641d87ac9d668cfabda6091a2c78b3666e921c
احمد يوسف صبحى محمد الجعرانى <108885178+ahmedyoussefg@users.noreply.github.com>
2024-03-14 14:07:35 +0200

    Add empty test case for bubble sort

--- src/SortArrayTest.java
+++ src/SortArrayTest.java
@@ -13,7 +13,7 @@ class SortArrayTest {
         SortArray sortArray = new SortArray(Arrays.asList(3, 1, 4, 1, 5, 9, 2, 6));
         List<List<Integer>> sortedLists = sortArray.simpleSort(false);
         List<Integer> expectedSorted = Arrays.asList(1, 1, 2, 3, 4, 5, 6, 9);
-        assertEquals(expectedSorted, sortedLists.get(sortedLists.size() - 1));
+        assertEquals(expectedSorted, sortedLists.get(0));
     }
     @Test
     void testOneBubbleSortWithSteps() {
@@ -33,5 +33,17 @@ class SortArrayTest {
 
         expectedSteps.add(Arrays.asList(1, 1, 2, 3, 4, 5, 6, 9));
         assertEquals(expectedSteps, actualSteps);
+    }
+    @Test
+    void EmptyTestCaseBubbleSort() {
+        SortArray sortArray = new SortArray(List.of());
+        List<List<Integer>> sortedLists = sortArray.simpleSort(false);
+        assertEquals(List.of(), sortedLists.get(0));
+    }
+    @Test
+    void EmptyTestCaseBubbleSortWithSteps() {
+        SortArray sortArray = new SortArray(List.of());
+        List<List<Integer>> sortedLists = sortArray.simpleSort(false);
+        assertEquals(List.of(List.of()), sortedLists);
     }
 }
